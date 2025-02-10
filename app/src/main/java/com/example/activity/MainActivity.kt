8effe5bd5f8b4332125e994defee0251a5713675
Talha <160234919+Talhaa21@users.noreply.github.com>
2025-02-10 15:16:08 +0500

    First Screen

---
+++ app/src/main/java/com/example/activity/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.RelativeLayout // Import RelativeLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val rootLayout = findViewById<RelativeLayout>(R.id.main) // Use RelativeLayout here
+        ViewCompat.setOnApplyWindowInsetsListener(rootLayout) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
