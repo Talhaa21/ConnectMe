43fabde80de125f0f08cc31a270ffc54e69b3ca9
Talha <160234919+Talhaa21@users.noreply.github.com>
2025-02-14 17:45:54 +0500

    Second Screen

--- app/src/main/java/com/example/activity/SecondScreenActivity.kt
+++ app/src/main/java/com/example/activity/SecondScreenActivity.kt
@@ -3,11 +3,9 @@ package com.example.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class SecondScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Change the layout to secondscreen.xml
-        setContentView(R.layout.fifthscreen)
+        setContentView(R.layout.secondscreen) // This should match your secondscreen.xml file
     }
 }
