37076861f1244672ada60c901fad8665aea09169
Talha <160234919+Talhaa21@users.noreply.github.com>
2025-02-15 11:45:07 +0500

    Tenth Screen

--- app/src/main/java/com/example/activity/MainActivity.kt
+++ app/src/main/java/com/example/activity/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         // Change the layout to secondscreen.xml
-        setContentView(R.layout.eleventhscreen)
+        setContentView(R.layout.twelvethscreen)
     }
 }
