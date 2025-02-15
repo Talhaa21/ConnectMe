091916dc9c151fecd48827ff04a6bdd3170f46a9
Talha <160234919+Talhaa21@users.noreply.github.com>
2025-02-15 20:50:53 +0500

    Seventeenth Screen

--- app/src/main/java/com/example/activity/MainActivity.kt
+++ app/src/main/java/com/example/activity/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         // Change the layout to secondscreen.xml
-        setContentView(R.layout.seventeenthscreen)
+        setContentView(R.layout.eighteenthscreen)
     }
 }
